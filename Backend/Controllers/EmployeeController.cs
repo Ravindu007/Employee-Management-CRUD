a88ee9647e4bfa41448bc2d917cacaf087d50209
Ravindu007 <ravindu0504@gmail.com>
2023-12-03 11:10:57 +0530

    GET, POST controllers added

---
+++ Backend/Controllers/EmployeeController.cs
@@ -0,0 +1,38 @@
+﻿using Backend.Data;
+using Backend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmployeeDbContext _dbcontext;
+
+        public EmployeeController(EmployeeDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetEmployees()
+        {
+            var employees = await _dbcontext.Employees.ToListAsync();
+            return Ok(employees);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateEmployee([FromBody] Employee employeeRequest)
+        {
+            employeeRequest.Id = Guid.NewGuid();
+            await _dbcontext.Employees.AddAsync(employeeRequest);
+            await _dbcontext.SaveChangesAsync();
+
+            return Ok(employeeRequest);
+        }
+    }
+}
